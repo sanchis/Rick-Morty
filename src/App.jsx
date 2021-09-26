a241e7654a81f88bbbbbf48158e6cf8ce2de6170
sanchis <sanchis1992@gmail.com>
2021-09-26 11:13:33 +0200

    feat: move to hook folder hash location

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { Route, Router } from 'wouter'
 import { CharactersContextProvider } from './context/CharactersContext'
 import Character from './pages/Character'
@@ -6,29 +6,7 @@ import CharacterList from './pages/CharacterList'
 import { ChakraProvider, Container, Image } from '@chakra-ui/react'
 import theme from './themes'
 import logo from '@/../assets/logo.png'
-
-// returns the current hash location in a normalized form
-// (excluding the leading '#' symbol)
-const currentLocation = () => {
-  return window.location.hash.replace(/^#/, '') || '/'
-}
-
-const navigate = (to) => (window.location.hash = to)
-
-const useHashLocation = () => {
-  const [loc, setLoc] = useState(currentLocation())
-
-  useEffect(() => {
-    // this function is called whenever the hash changes
-    const handler = () => setLoc(currentLocation())
-
-    // subscribe to hash changes
-    window.addEventListener('hashchange', handler)
-    return () => window.removeEventListener('hashchange', handler)
-  }, [])
-
-  return [loc, navigate]
-}
+import { useHashLocation } from './hooks/useHashLocation'
 
 export default function App () {
   return (
---
+++ src/hooks/useHashLocation.js
@@ -0,0 +1,25 @@
+// returns the current hash location in a normalized form
+
+import { useEffect, useState } from 'react'
+
+// (excluding the leading '#' symbol)
+const currentLocation = () => {
+  return window.location.hash.replace(/^#/, '') || '/'
+}
+
+const navigate = (to) => (window.location.hash = to)
+
+export const useHashLocation = () => {
+  const [loc, setLoc] = useState(currentLocation())
+
+  useEffect(() => {
+    // this function is called whenever the hash changes
+    const handler = () => setLoc(currentLocation())
+
+    // subscribe to hash changes
+    window.addEventListener('hashchange', handler)
+    return () => window.removeEventListener('hashchange', handler)
+  }, [])
+
+  return [loc, navigate]
+}
