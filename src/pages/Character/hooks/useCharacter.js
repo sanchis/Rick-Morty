5b767dc43e496a844847f904939a31ac9455d64a
sanchis <sanchis1992@gmail.com>
2021-09-26 11:54:18 +0200

    feat: move next move prev on character page

--- src/pages/Character/hooks/useCharacter.js
+++ src/pages/Character/hooks/useCharacter.js
@@ -1,10 +1,12 @@
 import { useContext, useEffect, useState } from 'react'
 import { CharactersContext } from '@/context/CharactersContext'
 import { getCharacter } from '../services'
+import { useLocation } from 'wouter'
 
 export function useCharacter (id) {
   const [loading, setLoading] = useState(false)
   const [character, setCharacter] = useState()
+  const [, setLocation] = useLocation()
   const {
     characters
   } = useContext(CharactersContext)
@@ -23,10 +25,20 @@ export function useCharacter (id) {
         .then(setCharacter)
         .finally(() => setLoading(false))
     }
-  }, [])
+  }, [id])
+
+  function moveNextCharacter () {
+    setLocation(`/character/${Number(id) + 1}`)
+  }
+
+  function movePrevCharacter () {
+    setLocation(`/character/${Number(id) - 1 === 0 ? id : Number(id) - 1}`)
+  }
 
   return {
     character,
-    loading
+    loading,
+    moveNextCharacter,
+    movePrevCharacter
   }
 }
