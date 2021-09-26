617f0dcd76be502c5d8bce20ed5ef470df6fe630
sanchis <sanchis1992@gmail.com>
2021-09-26 11:22:46 +0200

    feat: add icons

--- src/pages/Character/index.jsx
+++ src/pages/Character/index.jsx
@@ -4,6 +4,7 @@ import { useCharacter } from './hooks/useCharacter'
 import Card from '@/components/Card'
 import CharacterSummary from '@/components/CharacterSummary'
 import { useLocation } from 'wouter'
+import { ArrowBackIcon, ArrowLeftIcon, ArrowRightIcon } from '@chakra-ui/icons'
 
 export default function Character ({ id }) {
   const { character } = useCharacter(id)
@@ -13,10 +14,10 @@ export default function Character ({ id }) {
     <>
       <Card my='2' p='2'>
         <Flex justifyContent='space-between' direction={['column', 'column', 'row', 'row']}>
-          <Button my={['1', '1', '0']} onClick={() => setLocation('/')}>Back to the list</Button>
+          <Button leftIcon={<ArrowBackIcon />} my={['1', '1', '0']} onClick={() => setLocation('/')}>Back to the list</Button>
           <Flex justifyContent='space-between' direction={['column', 'column', 'row', 'row']}>
-            <Button my={['1', '1', '0']} mx='1'>Move prev character</Button>
-            <Button my={['1', '1', '0']} mx='1'>Move next character</Button>
+            <Button leftIcon={<ArrowLeftIcon />} my={['1', '1', '0']} mx='1'>Move prev character</Button>
+            <Button rightIcon={<ArrowRightIcon />} my={['1', '1', '0']} mx='1'>Move next character</Button>
           </Flex>
         </Flex>
       </Card>
--- src/pages/CharacterList/components/Paginator/index.jsx
+++ src/pages/CharacterList/components/Paginator/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Button, ButtonGroup } from '@chakra-ui/react'
 import { useCharactersFilter } from '../../hooks/useCharacterFilter'
+import { ArrowLeftIcon, ArrowRightIcon } from '@chakra-ui/icons'
 
 export default function Paginator () {
   const { moveNext, movePrev, canMoveNext, canMovePrev } = useCharactersFilter()
 
   return (
     <ButtonGroup colorScheme='primary' spacing='1' mx='1'>
-      <Button onClick={movePrev} disabled={!canMovePrev()}>Prev</Button>
-      <Button onClick={moveNext} disabled={!canMoveNext()}>Next</Button>
+      <Button leftIcon={<ArrowLeftIcon />} onClick={movePrev} disabled={!canMovePrev()}>Prev</Button>
+      <Button rightIcon={<ArrowRightIcon />} onClick={moveNext} disabled={!canMoveNext()}>Next</Button>
     </ButtonGroup>
   )
 }
