27d1b81bd9e4669535c396424b46216f4b1bb5b5
sanchis <sanchis1992@gmail.com>
2021-10-04 09:10:54 +0200

    feat: reuse selectors

--- cypress/integration/pages/Character.spec.js
+++ cypress/integration/pages/Character.spec.js
@@ -1,4 +1,8 @@
 /// <reference types="cypress" />
+
+import { getButtonBack, getButtonMoveNext, getButtonMovePrev, getCharacterContent, getLoadingIndicator } from '../../support/Character.selectors'
+import { getContainerList, getFilterInput } from '../../support/CharacterList.selectors'
+
 describe('Character page', () => {
   beforeEach(() => {
     cy.visit('#/character/1')
@@ -10,21 +14,21 @@ describe('Character page', () => {
 
   it('Character page can be display data of character', () => {
     cy.intercept('api/character/*', { fixture: 'character.json' }).as('getCharacter')
-    cy.get('[data-cy="character-content"] h2').first().should('have.text', 'Rick Sanchez')
-    cy.get('[data-cy="character-content"] p').should(($p) => {
+    getCharacterContent().get('h2').first().should('have.text', 'Rick Sanchez')
+    getCharacterContent().get('p').should(($p) => {
       expect($p.get(0).innerText).to.eq('Alive - Male Human')
       expect($p.get(1).innerText).to.eq('Location:Earth (Replacement Dimension)')
       expect($p.get(2).innerText).to.eq('Origin:Earth (C-137)')
     })
-    cy.get('[data-cy="character-content"] span').last().should('have.text', '4/10/2017 18:48')
+    getCharacterContent().get('span').last().should('have.text', '4/10/2017 18:48')
   })
 
   it('Character page loading indicator should be show', () => {
     cy.intercept('api/character/*', { fixture: 'character.json', delay: 3000 }).as('getCharacter')
-    cy.get('button[data-cy="navigate-next-character"]').click()
-    cy.get('[data-cy="loading-indicator"]').should('be.visible')
+    getButtonMoveNext().click()
+    getLoadingIndicator().should('be.visible')
     cy.wait(['@getCharacter'], { timeout: 10000 })
-    cy.get('[data-cy="loading-indicator"]').should('not.exist')
+    getLoadingIndicator().should('not.exist')
   })
 
   it('Character page not found can be opened a error page', () => {
@@ -38,15 +42,15 @@ describe('Character page', () => {
   })
 
   it('Character page can navigate to list', () => {
-    cy.get('button[data-cy="navigate-back-character"]').click()
-    cy.get('input[data-cy="filter-by-name"]').should('exist')
+    getButtonBack().click()
+    getFilterInput().should('exist')
   })
 
   it('Character page can navigate next', () => {
     cy.intercept('api/character/*', { fixture: 'character.json' }).as('getCharacter')
     cy.reload()
     cy.wait(['@getCharacter'])
-    cy.get('button[data-cy="navigate-next-character"]').click()
+    getButtonMoveNext().click()
     cy.wait(['@getCharacter']).then(intercept => {
       expect(intercept.request.url).to.match(/\/character\/2/)
     })
@@ -56,7 +60,7 @@ describe('Character page', () => {
     cy.intercept('api/character/*', { fixture: 'character.json' }).as('getCharacter')
     cy.visit('#/character/2')
     cy.wait(['@getCharacter'])
-    cy.get('button[data-cy="navigate-prev-character"]').click()
+    getButtonMovePrev().click()
     cy.wait(['@getCharacter']).then(intercept => {
       expect(intercept.request.url).to.match(/\/character\/1/)
     })
@@ -64,7 +68,7 @@ describe('Character page', () => {
 
   it('Character page from character list get character obj from context', () => {
     cy.visit('#/')
-    cy.get('[data-cy="container-list"]').children().first().click()
-    cy.get('[data-cy="loading-indicator"]').should('not.exist')
+    getContainerList().children().first().click()
+    getLoadingIndicator().should('not.exist')
   })
 })
