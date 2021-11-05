f1843bc337af4e127896d22109ce76d5c0878f71
Adrián Sanchis <adrian@devaway.es>
2021-11-05 21:05:56 +0100

    fix: refactor test to graphql

--- cypress/integration/pages/Character.spec.js
+++ cypress/integration/pages/Character.spec.js
@@ -1,26 +1,27 @@
 /// <reference types="cypress" />
 describe('Character page', () => {
   beforeEach(() => {
-    cy.visit('#/character/1')
+    cy.visit('#/character/4')
   })
 
   it('Character page can be opened', () => {
-    cy.contains('Rick Sanchez')
+    cy.contains('Beth Smith')
   })
 
   it('Character page can be display data of character', () => {
-    cy.intercept('api/character/*', { fixture: 'character.json' }).as('getCharacter')
+    cy.intercept('/graphql', { fixture: 'character.json' }).as('getCharacter')
+    cy.reload()
     cy.get('[data-cy="character-content"] h2').first().should('have.text', 'Rick Sanchez')
     cy.get('[data-cy="character-content"] p').should(($p) => {
       expect($p.get(0).innerText).to.eq('Alive - Male Human')
-      expect($p.get(1).innerText).to.eq('Location:Earth (Replacement Dimension)')
+      expect($p.get(1).innerText).to.eq('Location:Citadel of Ricks')
       expect($p.get(2).innerText).to.eq('Origin:Earth (C-137)')
     })
     cy.get('[data-cy="character-content"] span').last().should('have.text', '4/10/2017 18:48')
   })
 
   it('Character page loading indicator should be show', () => {
-    cy.intercept('api/character/*', { fixture: 'character.json', delay: 3000 }).as('getCharacter')
+    cy.intercept('/graphql').as('getCharacter')
     cy.get('button[data-cy="navigate-next-character"]').click()
     cy.get('[data-cy="loading-indicator"]').should('be.visible')
     cy.wait(['@getCharacter'], { timeout: 10000 })
@@ -43,28 +44,21 @@ describe('Character page', () => {
   })
 
   it('Character page can navigate next', () => {
-    cy.intercept('api/character/*', { fixture: 'character.json' }).as('getCharacter')
+    cy.intercept('/graphql').as('getCharacter')
     cy.reload()
     cy.wait(['@getCharacter'])
     cy.get('button[data-cy="navigate-next-character"]').click()
     cy.wait(['@getCharacter']).then(intercept => {
-      expect(intercept.request.url).to.match(/\/character\/2/)
+      expect(intercept.request.body.variables.id).to.eq('5')
     })
   })
 
   it('Character page can navigate prev', () => {
-    cy.intercept('api/character/*', { fixture: 'character.json' }).as('getCharacter')
-    cy.visit('#/character/2')
+    cy.intercept('/graphql').as('getCharacter')
     cy.wait(['@getCharacter'])
     cy.get('button[data-cy="navigate-prev-character"]').click()
     cy.wait(['@getCharacter']).then(intercept => {
-      expect(intercept.request.url).to.match(/\/character\/1/)
+      expect(intercept.request.body.variables.id).to.eq('3')
     })
-  })
-
-  it('Character page from character list get character obj from context', () => {
-    cy.visit('#/')
-    cy.get('[data-cy="container-list"]').children().first().click()
-    cy.get('[data-cy="loading-indicator"]').should('not.exist')
   })
 })
--- cypress/integration/pages/CharacterList.spec.js
+++ cypress/integration/pages/CharacterList.spec.js
@@ -8,46 +8,46 @@ describe('Character List page', () => {
   })
 
   it('CharacterList page can be filter by name', () => {
+    cy.intercept('/graphql').as('getCharacters')
     cy.get('input[data-cy="filter-by-name"]').type('random name ')
+    cy.wait('@getCharacters')
     cy.get('[data-cy="container-list"]').children().should('have.length', 0)
   })
 
   it('CharacterList page can be paginated next', () => {
-    cy.intercept('/api/character?name=&page=*', { fixture: 'characters.json' }).as('getCharacters')
-    cy.reload()
+    cy.intercept('/graphql', { fixture: 'characters.json' }).as('getCharacters')
     cy.wait('@getCharacters')
     cy.get('button[data-cy="filter-move-next"]').first().click()
     cy.wait('@getCharacters').then(intercept => {
-      expect(intercept.request.url).to.match(/\page=2/)
+      expect(intercept.request.body.variables.page).to.eq(2)
     })
   })
 
   it('CharacterList page disable buttons when page is loading', () => {
-    cy.intercept('/api/character?name=&page=*', { fixture: 'characters.json', delay: 5000 }).as('getCharacters')
+    cy.intercept('/graphql', { fixture: 'characters.json', delay: 25000 }).as('getCharacters')
     cy.reload()
     cy.get('button[data-cy="filter-move-prev"]').first().should('be.disabled')
     cy.get('button[data-cy="filter-move-next"]').first().should('be.disabled')
   })
 
   it('CharacterList page can be paginated prev', () => {
-    cy.intercept('api/character?*', { fixture: 'characters-prev.json' }).as('getCharacters')
-    cy.reload()
+    cy.intercept('/graphql', { fixture: 'characters-prev.json' }).as('getCharacters')
     cy.wait('@getCharacters')
     cy.get('button[data-cy="filter-move-next"]').first().click()
     cy.wait(['@getCharacters'])
     cy.get('button[data-cy="filter-move-prev"]').first().click()
     cy.wait(['@getCharacters']).then(intercept => {
-      expect(intercept.request.url).to.match(/\page=1/)
+      expect(intercept.request.body.variables.page).to.eq(1)
     })
   })
 
   it('CharacterList page paginate prev buttons can be disabled', () => {
-    cy.intercept('api/character?*', { fixture: 'characters.json' }).as('getCharacters')
+    cy.intercept('/graphql', { fixture: 'characters.json' }).as('getCharacters')
     cy.get('button[data-cy="filter-move-prev"]').first().should('be.disabled')
   })
 
   it('CharacterList page paginate next buttons can be disabled', () => {
-    cy.intercept('api/character?*', { fixture: 'charactersListLimit.json' }).as('getCharacters')
+    cy.intercept('/graphql', { fixture: 'charactersListLimit.json' }).as('getCharacters')
     cy.reload()
     cy.get('button[data-cy="filter-move-next"]').first().should('be.disabled')
   })
